4759101d03e3c0dded385fb2498eddf7f3d899dc
Quyet Vu <quyetvv_55@vnu.edu.vn>
2018-05-30 23:18:33 +0700

    add UI elements, show the value of slider

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue :Int = 0;
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currentValue = lroundf(slider.value)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +24,20 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func showAlert() {
+        let message = "The value of slider is: \(currentValue)"
+        let alert = UIAlertController(title: "Hello world", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+        print(currentValue)
+    }
 }
 
