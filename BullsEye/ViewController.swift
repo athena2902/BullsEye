2a1df83c56de5e88f8be16214bcb22adb6f28ca8
Quyet Vu <quyetvv_55@vnu.edu.vn>
2018-06-03 09:33:40 +0700

    customize slider

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -23,6 +23,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.startNewGame()
+        
+        let thumnImageNormal = UIImage(named: "SliderThumb-Normal")!
+        slider.setThumbImage(thumnImageNormal, for: .normal)
+        
+        let thumbImageHighlighted = #imageLiteral(resourceName: "SliderThumb-Highlighted")
+        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        let trackLeftImage = UIImage(named: "SliderTrackLeft")!
+        let trackLeftResizable = trackLeftImage.resizableImage(withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        
+        let trackRightImage = UIImage(named: "SliderTrackRight")!
+        let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
     }
 
     override func didReceiveMemoryWarning() {
